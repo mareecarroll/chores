16aae07f57010af687e587caddc7628b83d948f7
Maree Carroll <maree.carroll@gmail.com>
2018-12-16 04:36:15 +1100

    feat: add model stubs

---
+++ chore.py
@@ -0,0 +1,8 @@
+"""The Chore class to hold name and frequency in days."""
+class Chore:
+
+    def __init__(self, name, frequency_days):
+        pass
+    
+    def __repr__(self):
+        pass
---
+++ housemate.py
@@ -0,0 +1,8 @@
+"""The Housemate class to hold name and email."""
+class Housemate:
+
+    def __init__(self, name, email):
+        pass
+    
+    def __repr__(self):
+        pass
